2c6fa8ef4efb01bb6082175678ea144715ec3b0c
Ederson <ederson.ufrgs@gmail.com>
2020-12-02 08:55:54 -0300

    add page portifolio

---
+++ src/pages/portifolio.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/Layout"
+import SEO from "../components/seo"
+
+const PortifolioPage = () => (
+  <Layout>
+    <SEO title="Portifolio" />
+    <h1>this is Portifolio</h1>
+    <p>Portifolio!</p>
+  </Layout>
+)
+
+export default PortifolioPage
