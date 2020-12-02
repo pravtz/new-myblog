05986d8140497fc554e89a39566f9ac970919cbc
Ederson <ederson.ufrgs@gmail.com>
2020-12-02 08:55:34 -0300

    add page contact

---
+++ src/pages/contact.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/Layout"
+import SEO from "../components/seo"
+
+const ContactPage = () => (
+  <Layout>
+    <SEO title="Contact" />
+    <h1>this is Contact</h1>
+    <p>Contact!</p>
+  </Layout>
+)
+
+export default ContactPage
