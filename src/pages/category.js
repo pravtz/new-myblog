7f8ab6c26105210695be4e8bb6376cb8134d10dd
Ederson <ederson.ufrgs@gmail.com>
2020-12-02 08:55:08 -0300

    add page category

---
+++ src/pages/category.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/Layout"
+import SEO from "../components/seo"
+
+const CategoryPage = () => (
+  <Layout>
+    <SEO title="Category" />
+    <h1>this is Category</h1>
+    <p>Category!</p>
+  </Layout>
+)
+
+export default CategoryPage
