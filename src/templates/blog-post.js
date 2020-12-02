a41970353ae9849c98e6f2ed4fbdc445902a68e3
Ederson <ederson.ufrgs@gmail.com>
2020-12-02 08:52:54 -0300

    create pages of posts

---
+++ src/templates/blog-post.js
@@ -0,0 +1,44 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import Layout from "../components/Layout"
+import SEO from "../components/seo"
+import * as S from "../styles/styled-post"
+
+const BlogPost = ({ data }) => {
+  const post = data.markdownRemark
+
+  return (
+    <Layout>
+      <SEO
+        title={post.frontmatter.title}
+        description={post.frontmatter.description}
+      />
+      <S.PostHeader>
+        <S.PostDate>{post.frontmatter.date}</S.PostDate>
+        <S.PostTitle>{post.frontmatter.title}</S.PostTitle>
+        <S.PostDescription>{post.frontmatter.description}</S.PostDescription>
+      </S.PostHeader>
+
+      <S.MainContent>
+        <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
+      </S.MainContent>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query Post($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        description
+        date(formatString: "DD [de] MMMM [de] YYYY", locale: "pt-br")
+        category
+      }
+      html
+    }
+  }
+`
+
+export default BlogPost
