e5db603c91b6581262725602a0d8c58ff807b1cc
Ederson <ederson.ufrgs@gmail.com>
2020-12-02 08:54:50 -0300

    add page about

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/Layout"
+import SEO from "../components/seo"
+
+const AboutPage = () => (
+  <Layout>
+    <SEO title="About" />
+    <h1>this is About</h1>
+    <p>about!</p>
+  </Layout>
+)
+
+export default AboutPage
