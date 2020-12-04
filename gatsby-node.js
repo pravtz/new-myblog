d6ce65ee1d155f422c47dcd045cd5186b34af285
Ederson <ederson.ufrgs@gmail.com>
2020-12-04 20:08:11 -0300

    attempt to correct the React-Hot-Loader warning: react-🔥-dom patch is not detected.

--- gatsby-node.js
+++ gatsby-node.js
@@ -50,3 +50,13 @@ exports.createPages = ({ graphql, actions }) => {
     })
   })
 }
+
+exports.onCreateWebpackConfig = ({ getConfig, stage }) => {
+  const config = getConfig()
+  if (stage.startsWith("develop") && config.resolve) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "react-dom": "@hot-loader/react-dom",
+    }
+  }
+}
